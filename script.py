260785d08d4a1fbaaf95d312a1b31720ebea3c0f
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-10-17 13:59:12 +0200

    Aggunte le analisi dati su rover edge e plast

--- script.py
+++ script.py
@@ -73,6 +73,8 @@ df_1000045424 = df_1000045424.withColumn("Serial_Number", lit("1000045424"))
 df_1000045424 = df_1000045424.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
               "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
 
+df_rover_bft = df_rover_bft.union(df_1000045424)
+
 PATH = 'Dataset/Rover_B_FT_report/1000045962/logbiesse1000045962.csv'
 
 df_1000045962 = spark.read.option("header",True).csv(PATH)
@@ -111,6 +113,13 @@ df_rover_bft.groupBy("Serial_Number", "numTP") \
             .withColumnRenamed("max(VibromPeak)","VibromPeak max") \
             .show()
 
+#Calcolo del massimo di VibromPeak per ogni numTP
+
+df_rover_bft.groupBy("numTP") \
+            .max("VibromPeak") \
+            .withColumnRenamed("max(VibromPeak)","VibromPeak max") \
+            .show()
+
 #Calcolo del massimo di VibromPeak per ogni Serial_Number
 
 df_rover_bft.groupBy("Serial_Number") \
@@ -124,6 +133,19 @@ df_rover_bft.groupBy("Serial_Number") \
             .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
             .show()
 
+#Calcolo della mediana per ogni numTP
+
+df_rover_bft.groupBy("numTP") \
+            .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
+            .show()
+
+#Calcolo della deviazione standard per ogni numTP
+
+df_rover_bft.groupBy("numTP") \
+            .agg({'VibromPeak': 'stddev'}) \
+            .withColumnRenamed("stddev(VibromPeak)","VibromPeak stdDev") \
+            .show()
+
 #Calcolo della deviazione standard per ogni serial number
 
 df_rover_bft.groupBy("Serial_Number") \
@@ -131,9 +153,416 @@ df_rover_bft.groupBy("Serial_Number") \
             .withColumnRenamed("stddev(VibromPeak)","VibromPeak stdDev") \
             .show()
 
+#Calcolo della deviazione standard per ogni Serial number e numTP
+
+df_rover_bft.groupBy("Serial_Number", "numTP") \
+            .agg({'VibromPeak': 'stddev'}) \
+            .withColumnRenamed("stddev(VibromPeak)","VibromPeak stdDev") \
+            .show()
+
+#Calcolo della mediana per ogni Serial Number e numTP
+
+df_rover_bft.groupBy("Serial_Number", "numTP") \
+            .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
+            .show()
+
 print("----Fine analisi dati Rover_BF_T----\n")
+
+del df_1000037637
+del df_1000041225
+del df_1000041459
+del df_1000041964
+del df_1000041967
+del df_1000042109
+del df_1000043951
+del df_1000045424
+del df_1000045962
+del df_1000046482
+del df_rover_bft
+
 #-------------------------------------Fine analisi Rover_BF_T-----------------------------------------------------------
 
 
 
 
+
+
+
+
+
+
+
+
+
+#-------------------------------------Analisi dati su Rover_Edge---------------------------------------------------------
+
+print("\n")
+print("----Analisi dati su Rover_Edge----\n")
+
+PATH = 'Dataset/Rover_Edge_report/1000036664/logbiesse1000036664.csv'
+
+df_1000036664 = spark.read.option("header",True).csv(PATH)
+df_1000036664 = df_1000036664.withColumn("Serial_Number", lit("1000036664"))
+df_1000036664 = df_1000036664.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+PATH = 'Dataset/Rover_Edge_report/1000040598/logbiesse1000040598.csv'
+
+df_1000040598 = spark.read.option("header",True).csv(PATH)
+df_1000040598 = df_1000040598.withColumn("Serial_Number", lit("1000040598"))
+df_1000040598 = df_1000040598.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_1000036664.union(df_1000040598)
+
+PATH = 'Dataset/Rover_Edge_report/1000041415/logbiesse1000041415.csv'
+
+df_1000041415 = spark.read.option("header",True).csv(PATH)
+df_1000041415 = df_1000041415.withColumn("Serial_Number", lit("1000041415"))
+df_1000041415 = df_1000041415.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_rover_edge.union(df_1000041415)
+
+PATH = 'Dataset/Rover_Edge_report/1000042184/logbiesse1000042184.csv'
+
+df_1000042184 = spark.read.option("header",True).csv(PATH)
+df_1000042184 = df_1000042184.withColumn("Serial_Number", lit("1000042184"))
+df_1000042184 = df_1000042184.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_rover_edge.union(df_1000042184)
+
+PATH = 'Dataset/Rover_Edge_report/1000043379/logbiesse1000043379.csv'
+
+df_1000043379 = spark.read.option("header",True).csv(PATH)
+df_1000043379 = df_1000043379.withColumn("Serial_Number", lit("1000043379"))
+df_1000043379 = df_1000043379.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_rover_edge.union(df_1000043379)
+
+PATH = 'Dataset/Rover_Edge_report/1000044966/logbiesse1000044966.csv'
+
+df_1000044966 = spark.read.option("header",True).csv(PATH)
+df_1000044966 = df_1000044966.withColumn("Serial_Number", lit("1000044966"))
+df_1000044966 = df_1000044966.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_rover_edge.union(df_1000044966)
+
+PATH = 'Dataset/Rover_Edge_report/1000045965/logbiesse1000045965.csv'
+
+df_1000045965 = spark.read.option("header",True).csv(PATH)
+df_1000045965 = df_1000045965.withColumn("Serial_Number", lit("1000045965"))
+df_1000045965 = df_1000045965.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_rover_edge.union(df_1000045965)
+
+PATH = 'Dataset/Rover_Edge_report/1000046506/logbiesse1000046506.csv'
+
+df_1000046506 = spark.read.option("header",True).csv(PATH)
+df_1000046506 = df_1000046506.withColumn("Serial_Number", lit("1000046506"))
+df_1000046506 = df_1000046506.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_rover_edge.union(df_1000046506)
+
+PATH = 'Dataset/Rover_Edge_report/1000047674/logbiesse1000047674.csv'
+
+df_1000047674 = spark.read.option("header",True).csv(PATH)
+df_1000047674 = df_1000047674.withColumn("Serial_Number", lit("1000047674"))
+df_1000047674 = df_1000047674.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_rover_edge.union(df_1000047674)
+
+PATH = 'Dataset/Rover_Edge_report/1000049364/logbiesse1000049364.csv'
+
+df_1000049364 = spark.read.option("header",True).csv(PATH)
+df_1000049364 = df_1000049364.withColumn("Serial_Number", lit("1000049364"))
+df_1000049364 = df_1000049364.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_edge = df_rover_edge.union(df_1000049364)
+
+#Filtraggio delle righe null di VibromPeak e numTP
+
+df_rover_edge = df_rover_edge.filter(df_rover_edge.VibromPeak.isNotNull())
+df_rover_edge = df_rover_edge.filter(df_rover_edge.numTP.isNotNull())
+
+#Cast della colonna VibromPEak da string a double
+
+df_rover_edge = df_rover_edge.withColumn("VibromPeak", df_rover_edge["VibromPeak"].cast("double"))
+
+#Calcolo del massimo di VibromPeak per tutti i Rover Edge
+
+max_rover_edge_vibromPeak = df_rover_edge.select(max("VibromPeak")).collect()[0][0]
+
+#Calcolo del massimo di VibromPeak per ogni Serial_Number e numTP
+
+df_rover_edge.groupBy("Serial_Number", "numTP") \
+            .max("VibromPeak") \
+            .withColumnRenamed("max(VibromPeak)","VibromPeak max") \
+            .show()
+
+#Calcolo del massimo di VibromPeak per ogni numTP
+
+df_rover_edge.groupBy("numTP") \
+            .max("VibromPeak") \
+            .withColumnRenamed("max(VibromPeak)","VibromPeak max") \
+            .show()
+
+#Calcolo del massimo di VibromPeak per ogni Serial_Number
+
+df_rover_edge.groupBy("Serial_Number") \
+            .max("VibromPeak") \
+            .withColumnRenamed("max(VibromPeak)","VibromPeak max") \
+            .show()
+
+#Calcolo della mediana per ogni numTP
+
+df_rover_edge.groupBy("numTP") \
+            .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
+            .show()
+
+#Calcolo della deviazione standard per ogni numTP
+
+df_rover_edge.groupBy("numTP") \
+            .agg({'VibromPeak': 'stddev'}) \
+            .withColumnRenamed("stddev(VibromPeak)","VibromPeak stdDev") \
+            .show()
+
+#Calcolo della mediana per ogni serial number
+
+df_rover_edge.groupBy("Serial_Number") \
+            .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
+            .show()
+
+#Calcolo della deviazione standard per ogni serial number
+
+df_rover_edge.groupBy("Serial_Number") \
+            .agg({'VibromPeak': 'stddev'}) \
+            .withColumnRenamed("stddev(VibromPeak)","VibromPeak stdDev") \
+            .show()
+
+#Calcolo della deviazione standard per ogni Serial number e numTP
+
+df_rover_edge.groupBy("Serial_Number", "numTP") \
+            .agg({'VibromPeak': 'stddev'}) \
+            .withColumnRenamed("stddev(VibromPeak)","VibromPeak stdDev") \
+            .show()
+
+#Calcolo della mediana per ogni Serial Number e numTP
+
+df_rover_edge.groupBy("Serial_Number", "numTP") \
+            .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
+            .show()
+
+print("----Fine analisi dati Rover_Edge----\n")
+
+del df_1000036664
+del df_1000040598
+del df_1000041415
+del df_1000042184
+del df_1000043379
+del df_1000044966
+del df_1000045965
+del df_1000046506
+del df_1000047674
+del df_1000049364
+del df_rover_edge
+
+#-------------------------------------Fine analisi Rover_Edge-----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------Analisi dati su Rover_Plast---------------------------------------------------------
+
+print("\n")
+print("----Analisi dati su Rover_Plast----\n")
+
+PATH = 'Dataset/Rover_Plast_report/1000040499/logbiesse1000040499.csv'
+
+df_1000040499 = spark.read.option("header",True).csv(PATH)
+df_1000040499 = df_1000040499.withColumn("Serial_Number", lit("1000040499"))
+df_1000040499 = df_1000040499.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+PATH = 'Dataset/Rover_Plast_report/1000040499/logbiesse1000040499.csv'
+
+df_1000041221 = spark.read.option("header",True).csv(PATH)
+df_1000041221 = df_1000041221.withColumn("Serial_Number", lit("1000041221"))
+df_1000041221 = df_1000041221.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_1000040499.union(df_1000041221)
+
+PATH = 'Dataset/Rover_Plast_report/1000041541/logbiesse1000041541.csv'
+
+df_1000041541 = spark.read.option("header",True).csv(PATH)
+df_1000041541 = df_1000041541.withColumn("Serial_Number", lit("1000041541"))
+df_1000041541 = df_1000041541.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_rover_plast.union(df_1000041541)
+
+PATH = 'Dataset/Rover_Plast_report/1000041551/logbiesse1000041551.csv'
+
+df_1000041551 = spark.read.option("header",True).csv(PATH)
+df_1000041551 = df_1000041551.withColumn("Serial_Number", lit("1000041551"))
+df_1000041551 = df_1000041551.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_rover_plast.union(df_1000041551)
+
+PATH = 'Dataset/Rover_Plast_report/1000041934/logbiesse1000041934.csv'
+
+df_1000041934 = spark.read.option("header",True).csv(PATH)
+df_1000041934 = df_1000041934.withColumn("Serial_Number", lit("1000041934"))
+df_1000041934 = df_1000041934.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_rover_plast.union(df_1000041934)
+
+PATH = 'Dataset/Rover_Plast_report/1000042898/logbiesse1000042898.csv'
+
+df_1000042898 = spark.read.option("header",True).csv(PATH)
+df_1000042898 = df_1000042898.withColumn("Serial_Number", lit("1000042898"))
+df_1000042898 = df_1000042898.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_rover_plast.union(df_1000042898)
+
+PATH = 'Dataset/Rover_Plast_report/1000044949/logbiesse1000044949.csv'
+
+df_1000044949 = spark.read.option("header",True).csv(PATH)
+df_1000044949 = df_1000044949.withColumn("Serial_Number", lit("1000044949"))
+df_1000044949 = df_1000044949.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_rover_plast.union(df_1000044949)
+
+PATH = 'Dataset/Rover_Plast_report/1000045111/logbiesse1000045111.csv'
+
+df_1000045111 = spark.read.option("header",True).csv(PATH)
+df_1000045111 = df_1000045111.withColumn("Serial_Number", lit("1000045111"))
+df_1000045111 = df_1000045111.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_rover_plast.union(df_1000045111)
+
+PATH = 'Dataset/Rover_Plast_report/1000046652/logbiesse1000046652.csv'
+
+df_1000046652 = spark.read.option("header",True).csv(PATH)
+df_1000046652 = df_1000046652.withColumn("Serial_Number", lit("1000046652"))
+df_1000046652 = df_1000046652.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_rover_plast.union(df_1000046652)
+
+PATH = 'Dataset/Rover_Plast_report/1000047557/logbiesse1000047557.csv'
+
+df_1000047557 = spark.read.option("header",True).csv(PATH)
+df_1000047557 = df_1000047557.withColumn("Serial_Number", lit("1000047557"))
+df_1000047557 = df_1000047557.select("Serial_Number", "VibromPeak", "UTE", "ist_acc", "bearingsCelsius", "rpmSetPoint", "statoRot", "statorCelsius", "spindleRpm", 
+              "@timestamp", "numTP", "CurrentPeak", "DIAM", "LUNG", "idChiave")
+
+df_rover_plast = df_rover_plast.union(df_1000047557)
+
+#Filtraggio delle righe null di VibromPeak e numTP
+
+df_rover_plast = df_rover_plast.filter(df_rover_plast.VibromPeak.isNotNull())
+df_rover_plast = df_rover_plast.filter(df_rover_plast.numTP.isNotNull())
+
+#Cast della colonna VibromPeak da string a double
+
+df_rover_plast = df_rover_plast.withColumn("VibromPeak", df_rover_plast["VibromPeak"].cast("double"))
+
+#Calcolo del massimo di VibromPeak per tutti i Rover Edge
+
+max_rover_plast_vibromPeak = df_rover_plast.select(max("VibromPeak")).collect()[0][0]
+
+#Calcolo del massimo di VibromPeak per ogni Serial_Number e numTP
+
+df_rover_plast.groupBy("Serial_Number", "numTP") \
+            .max("VibromPeak") \
+            .withColumnRenamed("max(VibromPeak)","VibromPeak max") \
+            .show()
+
+#Calcolo del massimo di VibromPeak per ogni numTP
+
+df_rover_plast.groupBy("numTP") \
+            .max("VibromPeak") \
+            .withColumnRenamed("max(VibromPeak)","VibromPeak max") \
+            .show()
+
+#Calcolo del massimo di VibromPeak per ogni Serial_Number
+
+df_rover_plast.groupBy("Serial_Number") \
+            .max("VibromPeak") \
+            .withColumnRenamed("max(VibromPeak)","VibromPeak max") \
+            .show()
+
+#Calcolo della mediana per ogni numTP
+
+df_rover_plast.groupBy("numTP") \
+            .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
+            .show()
+
+#Calcolo della deviazione standard per ogni numTP
+
+df_rover_plast.groupBy("numTP") \
+            .agg({'VibromPeak': 'stddev'}) \
+            .withColumnRenamed("stddev(VibromPeak)","VibromPeak stdDev") \
+            .show()
+
+#Calcolo della mediana per ogni serial number
+
+df_rover_plast.groupBy("Serial_Number") \
+            .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
+            .show()
+
+#Calcolo della deviazione standard per ogni Serial number e numTP
+
+df_rover_plast.groupBy("Serial_Number", "numTP") \
+            .agg({'VibromPeak': 'stddev'}) \
+            .withColumnRenamed("stddev(VibromPeak)","VibromPeak stdDev") \
+            .show()
+
+#Calcolo della mediana per ogni Serial Number e numTP
+
+df_rover_plast.groupBy("Serial_Number", "numTP") \
+            .agg(expr('percentile(VibromPeak, array(0.5))')[0].alias("VibromPeak median")) \
+            .show()
+
+print("----Fine analisi dati Rover_Plast----\n")
+
+del df_1000040499
+del df_1000041221
+del df_1000041541
+del df_1000041551
+del df_1000041934
+del df_1000042898
+del df_1000044949
+del df_1000045111
+del df_1000046652
+del df_1000047557
+del df_rover_plast
+
+#-------------------------------------Fine analisi Rover_Plast-----------------------------------------------------------
